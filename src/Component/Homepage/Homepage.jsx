0b8cc759f5f343aa584ecd4a3f78fe6e0ce02200
Sakshi <sakshisharma01.30@gmail.com>
2025-12-16 16:27:03 +0530

    feat: Enhance Homepage with Hero1 component and update Hero layout

--- src/Component/Homepage/Homepage.jsx
+++ src/Component/Homepage/Homepage.jsx
@@ -3,12 +3,14 @@ import './Homepage.css';
 import Hero from './Hero/Hero';
 import Discount from './Discount/Discount';
 import Carousel from './Carousel/Carousel';
+import Hero1 from './Hero1/Hero1';
 
 function Homepage() {
     return (
         <div>
             <Hero />
             <Carousel />
+            <Hero1 />
             <Discount />
         </div>
     )
